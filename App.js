4b5651fc3f5222159ae5b28a3240bf16c6638791
Shakti <1999shaktirajsingh@gmail.com>
2024-01-26 23:19:04 +0530

    Using npm for package management

---
+++ App.js
@@ -0,0 +1,12 @@
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", { id: "heading" }, "I'm a H1 Tag")
+  )
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
